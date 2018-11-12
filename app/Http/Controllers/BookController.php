a8c110d85873f3fd051e309ee3e9d8d728e3c0f0
Rodrigo Maia <rodrigoasmaia@gmail.com>
2018-11-11 22:22:22 -0200

    Exibindo lista de livros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -19,19 +19,35 @@ class BookController extends Controller
         $this->categoryCtr = new CategoryController();//Instancia Controlador de categorias
     }
     
+    private static function str_resume($texto,$tam){
+        $resumo = substr($texto,'0',$tam); //Cortar a string no numero de casas definido
+	$lastPos = strrpos($resumo," ");//Busca a posição do ultimo caracter vazio
+	$resumo = substr($resumo,0,$lastPos);//Corta a String da posição 0 até o ultimo caracter vazio
+	
+        return $resumo."...";
+    }
     
     public function byCategory($catID = null){       
         $categories = $this->categoryCtr->getCategories();//recuperando categorias
-        
-        
+                   
         if($catID == null){//Se não tiver nenhuma categoria selecionada coloca todas na tela
-           $books = $this->book->all();
+           $getBooks = $this->book->all();
+           $title_body ="Home";
         }else{//buscando livros por categoria
             $category = $this->categoryCtr->getCategory($catID); //Recuperando categoria selecionada;            
-            $books = $category->books($catID);//Recuperando os livros por categoria           
+            $getBooks = $category->books($catID);//Recuperando os livros por categoria  
+            $title_body = "Category > ".$category['CategoryName'];
+        }  
+        
+        $books = array();//Guardando dados em um array padronizado para as duas buscas
+        foreach($getBooks as $book){//Resumindo descrições
+            $book['description'] = $this::str_resume($book['description'],120); 
+            $books[] = $book;
         }
         
-        return view("books_view/books_view",compact("categories","books","catID"));
+        
+
+        return view("books_view/books_view",compact("categories","books","title_body"));
     }
     
     
@@ -40,8 +56,14 @@ class BookController extends Controller
         $categories = $this->categoryCtr->getCategories();//recuperando categorias
         
         $keyWord = $post['search_books'];
-        $books = $this->book->where('title','LIKE','%'.$keyWord.'%')->get();
-
-        return view("books_view/books_view",compact("categories","books","keyWord"));
+        $books = $this->book->where('title','LIKE','%'.$keyWord.'%')->get();                
+                
+        foreach($books as $book){//Resumindo descrições
+            $book['description'] = $this::str_resume($book['description'],120);
+        }
+        
+        $title_body = "Search > '".$keyWord."'";
+   
+        return view("books_view/books_view",compact("categories","books","keyWord","title_body"));
     }
 }
--- resources/views/books_view/books_view.blade.php
+++ resources/views/books_view/books_view.blade.php
@@ -1,15 +1,27 @@
 @include('../templates/headerbase')
 
-        <div class='container'>
-            <ul>
-                @foreach($books as $book)
-                <li>
-                  
-                        {{$book["title"]}}
-                   
-                </li>
-                @endforeach
-            </ul>
+        <div class='container container-body'>
+            
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <h1><small>{{$title_body}}</small></h1>
+                </div>
+            </div>
+            
+            
+            <div class="row">
+            @foreach($books as $book)           
+                    <div class="col-sm-6 col-md-3">
+                      <div class="thumbnail">
+                        <img  class="img-rounded" src="{{'http://yorktown.cbe.wwu.edu/sandvig/mis314/assignments/bookstore/bookimages/'.$book["ISBN"].'.01.THUMBZZZ.jpg'}}" alt="{{$book["title"]}}">
+                        <div class="caption">
+                          <h3>{{$book["title"]}}</h3>
+                          <p>{{$book["description"]}}<a>mais</a></p>                          
+                        </div>
+                      </div>
+                    </div>
+            @endforeach
+            </div>
         </div>
 
     </body>
