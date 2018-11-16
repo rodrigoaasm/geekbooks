94a03caf51ba61c21358fc6aaa4e26ff8cdefc99
LucasLamounierD <lucaslamounier13@gmail.com>
2018-11-16 14:06:33 -0200

    Commit para merge

---
+++ resources/views/user_view/form_user.blade.php
@@ -0,0 +1,13 @@
+
+    {{--<h1>Nova Notícia</h1>--}}
+    {{--<form action="" method="post">--}}
+        {{--<input type="hidden" name="_token" value="{{{ csrf_token() }}}"/>--}}
+        {{--<div class="form-group"><label>Email:</label> <input class="form-control" name="email"></div>--}}
+        {{--<div class="form-group"><label>First Name:</label> <input class="form-control" name="fname"></div>--}}
+        {{--<div class="form-group"><label>Last Name:</label> <input class="form-control" name="lname"></div>--}}
+        {{--<div class="form-group"><label>Street:</label> <input class="form-control" name="street"></div>--}}
+        {{--<div class="form-group"><label>City:</label> <input class="form-control" name="city"></div>--}}
+        {{--<div class="form-group"><label>State:</label> <input class="form-control" name="state"></div>--}}
+        {{--<div class="form-group"><label>Zip:</label> <input class="form-control" name="zip"></div>--}}
+        {{--<button type="submit" class="btn btn-primary btn-block">Submit</button>--}}
+    {{--</form>--}}
---
+++ resources/views/user_view/login.blade.php
@@ -0,0 +1,7 @@
+
+    {{--<h1>Digite seu email:</h1>--}}
+    {{--<form action="" method="post">--}}
+        {{--<input type="hidden" name="_token" value="{{{ csrf_token() }}}"/>--}}
+        {{--<div class="form-group"><label>Email:</label> <input class="form-control" name="email"/></div>--}}
+        {{--<button type="submit" class="btn btn-primary btn-block">Submit</button>--}}
+    {{--</form>--}}
