1d1b06371a704f3cf1de80996fa00ade551a2179
kevin74Unifei <kevin.vieira@yahoo.com.br>
2018-11-19 16:16:36 -0200

    Patch: Carrinho permite digitar multiplos numeros

--- public/js/cart_view.js
+++ public/js/cart_view.js
@@ -1,9 +1,8 @@
  //Adiciona um listener para cada item de quantidade
  //Quando sofrer alteração irá realizar o submit do form de atualização do item selecionado
 $(document).ready(function(){    
-    $('.cart_qty').on('keyup',function(){
+    $('.cart_qty').on('blur',function(){
         var id = $(this).attr('id');
         $('#updateCart-'+id).submit();
     });
 });        
-
