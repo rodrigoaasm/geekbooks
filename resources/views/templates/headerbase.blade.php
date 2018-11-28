2ade1cff6d19f7a0ecb0c6926e39be1554be63b0
Rodrigo Maia <rodrigoasmaia@gmail.com>
2018-11-28 15:30:34 -0200

    Resolução de alguns issues, melhoramento do menu-side e resolução da duplicação de livros na pesquisa.

--- resources/views/templates/headerbase.blade.php
+++ resources/views/templates/headerbase.blade.php
@@ -42,8 +42,8 @@
                         <a class="cart-button" href="{{url('/cart/show')}}">
                             <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 "><!--Desenhando icon-->
                                 <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path>
-                            </svg> Link
-                            <span class="item-number ">0</span>
+                            </svg> Cart
+                            
                         </a>
                     </div>
                 </div>
@@ -77,15 +77,21 @@
                         <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path>
                     </svg> Link                     
                 </a>   
-                <div class="dropdown dropdown-side"><a class="links" href="#">Categories</a>
-                        <ul class="dropdown-menu">
+                <a class="links" href="{{url('/order/show')}}">Order History</a>
+                <div class="dropdown dropdown-side" id="dropdown-side">
+                        <a class="links" href="#">Categories</a>
+                        <!--<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                            Dropdown
+                            <span class="caret"></span>
+                          </button>-->
+                        <ul class="dropdown-menu" id="dropdown-side-menu">
                             <!--Interando categorias, para o menu menor-->
                             @foreach($categories as $cat)
                                 <li class="profile-li"><a class="profile-links" href="{{url('/').'/'.$cat['CategoryID']}}">{{$cat['CategoryName']}}</a></li>                            
                             @endforeach                            
                         </ul>
                 </div>                
-                <a class="links" href="{{url('/order/show')}}">Order History</a>
+                
             </div>
         
         </div>
